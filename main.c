1eb1b8cadc59458914273598e2d36e55556a749c
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-15 11:36:55 -0800

    main.c

--- main.c
+++ main.c
@@ -12,14 +12,14 @@ int main(void)
     int len;
     int len2;
     unsigned int ui;
-    /*void *addr;*/
+    void *addr;
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
-    /*addr = (void *)0x7ffe637541f0;*/
-    _printf("Length:[%d, %i]\n", INT_MIN, len);
-    printf("Length:[%d, %i]\n", INT_MIN, len2);
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
     _printf("Unsigned:[%u]\n", ui);
@@ -28,18 +28,17 @@ int main(void)
     printf("Unsigned octal:[%o]\n", ui);
     _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
     printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n",64);
-    printf("Character:[%c]\n", 64);
-    _printf("String:[%s]\n", "This is a string");
-    printf("String:[%s]\n", "This is a string");
-    /*_printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);*/
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-    _printf("ROT13:[%R]\n", "This is a string");
+    _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
-    _printf("%s\n", 0);
     return (0);
 }
