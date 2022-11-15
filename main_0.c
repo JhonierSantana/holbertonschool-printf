8da0f8d2d61ee12b35b44a6f7a6b72ca91caa48b
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-15 09:03:50 -0800

    main_0.c

---
+++ main_0.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _printf - printf a simple snetence
+ *
+ * @format - sentence
+ *
+ * Return 0
+ */
+int _printf(const char *format, ..)
+{
+	int s;
+	va_list ap;
+
+	if (format == NULL)
+		return (-1);
+	
+	s = _strlen(format);
+	if (s <= 0)
+		return (0);
+
+	va_star(ap, format);
+	s = arg(format, ap);
+
+	_puthcar (-1);
+	va_end(ap);
+
+	return (s);
+}
