ec4eb46cde8d0d54f56149d153aa243423c98626
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-16 08:51:06 -0800

    main.h

--- main.h
+++ main.h
@@ -4,9 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <limits.h>
+
+typedef struct print
+{
+	char *p;
+	void (*f)(va_list);
+} printf_f;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_c(va_list c);
 int print_c(va_list s);
+
 #endif
