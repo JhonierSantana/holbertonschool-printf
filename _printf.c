a97becbc6aa335615dc4d9c0bbef1b1be8c6115b
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-16 08:46:42 -0800

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+
+	va_list elements;
+    
+	char len;
+    int a = 0;
+
+    va_start(elements, format);
+    
+        
+        while (format[a])
+        {
+            a++;
+            _putchar(len);
+            len = *(format + a 0);
+            
+        }
+	_putchar('\n');
+    va_end(elements);
+	return(a);
+}
