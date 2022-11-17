7a764b4bc3cdcfcfb98ea8214fc00bc13395c7e9
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-17 05:48:13 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -5,19 +5,17 @@ int _printf(const char *format, ...)
 	int i = 0;
 	char len = 0;
 	va_list ap;
-	char buffer[2000];
+	char buffer[1048];
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0')
 		return (-1);
-	va_start(ap, format[i]);
+	va_start(ap, format);
 	while (format && format[i])
 	{
-		if (format[i] == '%')
-		{
+		if (format[i] != '%')
 			buffer[len] = format[i];
-			i++;
-		}	
 	}
+	i++;
 	write(1, buffer, len);
 	va_end(ap);
 	return (len);
