e18958bab8937b86a9c5454f9ba0adc7e3f96c20
Jhonier Santana <101937551+JhonierSantana@users.noreply.github.com>
2022-11-21 11:34:21 -0500

    Update get_func_print.c

---
+++ get_func_print.c
@@ -0,0 +1,19 @@
+// aca sera cada llamda de cada caso
+/*#include "main.h"
+
+int (*get_print_cases(char *arg))(va_list, char *, int) // funcion principal que se llamara en print.c con el nombre de la estructura
+{
+	int i = 0;
+	cases format[] = {
+		{"c", print_c}, // su respectivo puntero "c" y su respectivo nombre de funcion donde contiene el proceso
+		{"s", print_s},
+	};
+	while (i < 2) // numero de casos en el momento, van 2
+	{
+	    if (*cases == *(format[i])->print_cases) // aca en este while, aca accdemos a los datos de la estructura, por el cual
+			return ((format[i]->cases_print));  // asi meter los datos del caso y que pueda imprimir las respuestas
+		i++;
+	}
+	
+}
+*/
