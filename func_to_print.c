a1b0b92d5d027b1049e8d130e05072a678538acd
Jhonier Santana <101937551+JhonierSantana@users.noreply.github.com>
2022-11-22 11:28:18 -0500

    Update func_to_print.c

--- func_to_print.c
+++ func_to_print.c
@@ -2,10 +2,12 @@
 
 int print_c(va_list ap, char *buff, int len)
 {
-	char *c;
-
 	c = malloc((sizeof(char) + 1));
+	if (c == NULL)
+	    exit(1);
 	c[0] = va_arg(ap, int);
+	if (c[0] == 0)
+		exit (1);
 	len = replace(buff, c, len);
 	free(c);
 	return (len);
@@ -13,13 +15,17 @@ int print_c(va_list ap, char *buff, int len)
 
 int print_s(va_list ap, char *buff, int len)
 {
-    char *s;
+    	char *s;
 	char *a;
 	int len_s;
 
 	a = va_arg(ap, char*);
+	if (a == NULL)
+		    exit(1);
 	len_s = _strlen(a);
 	s = malloc((sizeof(char) * len_s) + 1);
+	if (s == NULL)
+		exit (1);
 	_strcpy(s, a);
 	len = replace(buff, s, len);
 	free(s);
