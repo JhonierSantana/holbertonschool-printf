38b71ec7cb0489bfd330b01d6bb0dc773f3892b8
Jhonier Santana <101937551+JhonierSantana@users.noreply.github.com>
2022-11-24 11:21:11 -0500

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -12,7 +12,7 @@ int _printf(char *format, ...)
 	
 	if (format == NULL || (format[0] == '%' && format[1] == '\0')
 	    || (format[0] == '\\' && format[1] == '\0'))
-		return (1);
+		return (-1);
 
 	while (format[a])
 	{
