0ca9f052b00f3f52adb6b92abce4a930ad8be091
Jhonier Santana <101937551+JhonierSantana@users.noreply.github.com>
2022-11-24 09:50:10 -0500

    Update main.h

--- main.h
+++ main.h
@@ -24,5 +24,6 @@ int replace (char *buffer, char *s, int print_len);
 int print_s (va_list ap, char *buff, int len);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
+int percent_case(va_list ap, char *buff, int len);
 
 #endif
