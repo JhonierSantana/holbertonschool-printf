af3352544dfdb8847f7d2e147656242e6dd802f1
Jhonier Santana <101937551+JhonierSantana@users.noreply.github.com>
2022-11-24 10:56:05 -0500

    Update func_to_print.c

--- func_to_print.c
+++ func_to_print.c
@@ -9,7 +9,7 @@ int print_c(va_list ap, char *buff, int len)
 	    return (len);
 	c[0] = va_arg(ap, int);
 	if (c[0] == 0)
-		exit (1);
+		return (1);
 	len = replace(buff, c, len);
 	free(c);
 	return (len);
@@ -39,7 +39,7 @@ int percent_case(va_list ap, char *buff, int len)
 	char *p;
 	p = malloc((sizeof(char) + 1));
 	if (p == NULL)
-		 exit(1);
+		 return (1);
 	p[0] = va_arg(ap, int);
 	p[0] = '%';
 	len = replace(buff, p, len);
