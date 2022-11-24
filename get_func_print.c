c33f161d1706d11a4fdc8dc49a6c996242b791f2
Jhonier Santana <101937551+JhonierSantana@users.noreply.github.com>
2022-11-24 09:39:27 -0500

    Update get_func_print.c

--- get_func_print.c
+++ get_func_print.c
@@ -5,8 +5,9 @@ int (*get_print_cases(char *arg))(va_list, char *, int)
 	cases format[] = {
 		{"c", print_c}, 
 		{"s", print_s},
+		{"%", percent_case},
 	};
-	while (i < 2) 
+	while (i < 3) 
 	{
 	    if (*arg == *(format[i]).print_cases)
 			return ((format[i].cases_print));
