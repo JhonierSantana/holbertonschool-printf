546f249048d6d6a3ad85ad905336a5a2d3732d3c
Jhonier Santana <101937551+JhonierSantana@users.noreply.github.com>
2022-11-25 07:41:55 -0500

    Create functions_integers.c

---
+++ functions_integers.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _itoa - Converts an intiger to specified argument
+ * @num: intiger to convert
+ * @s: Pointer to save int into
+ * @base: Base to convert int
+ * Return: Pointer to s
+ */
+
+char *_itoa(int num, char *s, unsigned int base)
+{
+	int i = 0;
+	int res;
+	int sign = 0;
+	unsigned int a;
+
+	if (num == 0)
+	{
+		s[i] = '0';
+		s[i + 1] = '\0';
+		return (s);
+	}
+	if (num < 0 && base == 10)
+	{
+		sign = 1;
+		a = num * (-1);
+	}
+	else
+		a = num;
+	while (a > 0)
+	{
+		res = a % base;
+		if (res > 9)
+			s[i] = (res - 10) + 'a';
+		else
+			s[i] = res + '0';
+		a = a / base;
+		i++;
+	}
+	if (sign == 1)
+	{
+		s[i] = '-';
+		i++;
+	}
+	s[i] = '\0';
+	rev_string(s);
+	return (s);
+}
