d43784e4e52d0f913978b9f1c9aaae4b9026f693
Jhonier Santana <101937551+JhonierSantana@users.noreply.github.com>
2022-11-25 08:14:00 -0500

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,42 +1,39 @@
 #include "main.h"
-
 int _printf(char *format, ...)
 {
-	size_t len = 0;
-	char buffer[2000];
-	va_list ap;
-	int a = 0;
-	int (*cases_print)(va_list, char *, int );
-	va_start (ap, format);
-	
-	if (format == NULL || (format[0] == '%' && format[1] == '\0')
-		|| (format[0] == ' ' && format[1] == '\0'))
-		return (-1);
-	
-	while (format[a])
-	{
-		if (format[a] != '%')
-		{
-	        buffer[len] = format[a];
-			len += 1;
-		}
-		else
-		{
-			cases_print = get_print_cases(&(format[a + 1]));
-			if (cases_print != NULL)
-		    {
-				len = cases_print(ap, &buffer[len], len);
-				a++;
-		    }
-		    else
-		    {
-			buffer[len] = format[a];
-				len += 1;
-			}
-		}
-		a++;
-	}
-	write(1, buffer, len);
-	va_end(ap);
-	return (len);
+size_t len = 0;
+char buffer[2000];
+va_list ap;
+int a = 0;
+int (*cases_print)(va_list, char *, int );
+va_start (ap, format);
+if (format == NULL || (format[0] == '%' && format[1] == '\0')
+|| (format[0] == ' ' && format[1] == '\0'))
+return (-1);
+while (format[a])
+{
+if (format[a] != '%')
+{
+buffer[len] = format[a];
+len += 1;
+}
+else
+{
+cases_print = get_print_cases(&(format[a + 1]));
+if (cases_print != NULL)
+{
+len = cases_print(ap, &buffer[len], len);
+a++;
+}
+else
+{
+buffer[len] = format[a];
+len += 1;
+}
+}
+a++;
+}
+write(1, buffer, len);
+va_end(ap);
+return (len);
 }
